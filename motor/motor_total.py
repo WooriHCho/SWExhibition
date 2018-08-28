dbd60b5ef56c5cf248c1398c971c25c215ef7111
woori <woori.helena.cho@gmail.com>
2018-08-27 21:42:59 -0900

    0828 motor control final

---
+++ motor/motor_total.py
@@ -0,0 +1,37 @@
+from __future__ import division
+import time
+import Adafruit_PCA9685
+pwm = Adafruit_PCA9685.PCA9685()
+
+servo_min = 100;
+servo_max = 200;
+
+def set_servo_pulse(channel, pulse):
+    pulse_length = 1000000    # 1,000,000 us per second
+    pulse_length //= 20       # 60 Hz
+    print('{0}us per period'.format(pulse_length))
+    pulse_length //= 4096     # 12 bits of resolution
+    print('{0}us per bit'.format(pulse_length))
+    pulse *= 1000
+    pulse //= pulse_length
+    pwm.set_pwm(channel, 0, pulse)
+
+# Set frequency to 60hz, good for servos.
+pwm.set_pwm_freq(60)
+
+i=0;
+print('Moving servo on channel 0, press Ctrl-C to quit...')
+while i < 10:
+    # Move servo on channel O between extremes.
+	pwm.set_pwm(4, 0, 2048)
+	pwm.set_pwm(5, 0, 2048)
+	time.sleep(1)
+	pwm.set_pwm(0, 0, servo_min)
+	time.sleep(1)
+	pwm.set_pwm(0, 0, servo_max)
+	time.sleep(1)
+	
+	i += 1
+	print i
+pwm.set_pwm(4, 0, 0)
+pwm.set_pwm(5, 0, 0)
